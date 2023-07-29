16773fcb6da69c8477cc70c89aae930658b42372
Nayan Washnik <128236231+NayanpR0@users.noreply.github.com>
2023-07-29 11:10:13 +0530

    Update config.py

--- tiktokbot/data/config.py
+++ tiktokbot/data/config.py
@@ -3,6 +3,6 @@ from environs import Env
 env = Env()
 env.read_env()
 # .env fayl chidan quyidagilarni o'qiymiz
-BOT_TOKEN = ""  # Bot token. go to https://t.me/BotFather to create bot and get token
-ADMINS = [] #ID of Admins. go to @ShowJsonBot (https://t.me/ShowJsonBot) to find out your ID
+BOT_TOKEN = "5436751182:AAFSW6kWJmIWc14C6fLNspX0ZezGW08TmB4"  # Bot token. go to https://t.me/BotFather to create bot and get token
+ADMINS = "5149937796" #ID of Admins. go to @ShowJsonBot (https://t.me/ShowJsonBot) to find out your ID
 
